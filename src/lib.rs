5e2e3c96938f0eded782a2b37582b5b7e25dc84a
LEGRAND Matthieu <legrand.ma@chu-toulouse.fr>
2023-04-03 15:04:06 +0200

    bumping shuttle version

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ pub mod wallpaper;
 mod routes;
 
 //Serve routes using shuttle
-#[shuttle_service::main]
-async fn warp() -> shuttle_service::ShuttleWarp<(impl Reply,)> {
+#[shuttle_runtime::main]
+async fn warp() -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
 
     let hello = warp::path!("hello" / String)
         .and(warp::get())
         .map(|name| format!("Hello, {}!", name));
     let route = hello.or(routes::wallpaper());
-    Ok(route.with(warp::log("wallpaper::api")).boxed())
+    Ok(route.with(warp::log("wallpaper::api")).boxed().into())
 }
